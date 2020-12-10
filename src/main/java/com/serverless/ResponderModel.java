36e6cb290200c71967463e42afe33b159723c409
jvillavicencio01 <jose.villavicencio@externos.sura.cl>
2020-12-10 20:03:21 +0000

    Se agrega segundo grupo de pruebas unitarias

--- src/main/java/com/serverless/ResponderModel.java
+++ src/main/java/com/serverless/ResponderModel.java
@@ -2,6 +2,7 @@ package com.serverless;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.lang.*;
 
 public class ResponderModel {
     private final String teamName = "Equipo1";
@@ -14,11 +15,25 @@ public class ResponderModel {
         if (nameMatcher.matches()){
             return teamName;
         }
-        Matcher sumMatcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(question);
+        Matcher sumMatcher = Pattern.compile(".*what is (\\d+) plus (\\d+)").matcher(question);
         if (sumMatcher.matches()) {
             return String.valueOf(Integer.parseInt(sumMatcher.group(1)) + Integer.parseInt(sumMatcher.group(2)));
         }
-        
+        Matcher maxMatcherOfTwo = Pattern.compile(".*which of the following numbers is the largest: (\\d+),(\\d+)").matcher(question);
+        if (maxMatcherOfTwo.matches()) {
+            if (Integer.parseInt(maxMatcherOfTwo.group(1))>Integer.parseInt(maxMatcherOfTwo.group(2))){
+                return String.valueOf(Integer.parseInt(maxMatcherOfTwo.group(1)));
+            }
+            else {
+                return String.valueOf(Integer.parseInt(maxMatcherOfTwo.group(2)));
+            }
+        }
+        Matcher maxMatcherOfFour = Pattern.compile(".*which of the following numbers is the largest: (\\d+),(\\d+),(\\d+),(\\d+)").matcher(question);
+        if (maxMatcherOfTwo.matches()) {
+            return "error";
+            //return String.valueOf(Math.max(Integer.parseInt(maxMatcherOfTwo.group(1)),Integer.parseInt(maxMatcherOfTwo.group(2)),Integer.parseInt(maxMatcherOfTwo.group(3)),Integer.parseInt(maxMatcherOfTwo.group(4))));
+        }
+
         return teamName;
     }
 
--- src/test/java/com/serverless/responderModelTest.java
+++ src/test/java/com/serverless/responderModelTest.java
@@ -6,13 +6,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class responderModelTest {
 
     @Test
-    public void  cuandoPreguntanPorElNombreRespondeCarlos(){
+    public void  cuandoPreguntanPorElNombreRespondeEquipo1(){
         ResponderModel responder = new ResponderModel();
         String pregunta = "f5d48e90: what is your name";
         String respuesta = responder.answer(pregunta);
         assertThat(respuesta).isEqualTo("Equipo1");
 
 
+    }
+    @Test
+    public void  cuandoPreguntanPorElNumeroMasAltoResponde107(){
+        ResponderModel responder = new ResponderModel();
+        String pregunta = "d131fef0: which of the following numbers is the largest: 107, 16";
+        String respuesta = responder.answer(pregunta);
+        assertThat(respuesta).isEqualTo("107");
+    }
+    @Test
+    public void  cuandoPreguntanporLaSumaDeDosValoresResponde(){
+        ResponderModel responder = new ResponderModel();
+        String pregunta ="d7ba3e20: what is 8 plus 1";
+        String respuesta = responder.answer(pregunta);
+        assertThat(respuesta).isEqualTo("9");
     }
 
 }
